fb5ca87aeb5f60e30d65e25bdcd46d95b6c5196b
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-30 14:48:49 +0900

    SPACE

--- events/onMember.js
+++ events/onMember.js
@@ -2,5 +2,5 @@
  * @param {import('discord.js').GuildMember} member
  */
 module.exports = (member) => {
-  member.setNickname('[ 유저 ]' + (member.nickname || member.user.username))
+  member.setNickname('[ 유저 ] ' + (member.nickname || member.user.username))
 }
