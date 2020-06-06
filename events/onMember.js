afec1da28cda85715b4f87232345cecabf917108
pmh-only <pmhstudio.pmh@gmail.com>
2020-06-06 16:42:27 +0900

    Auto Role Add

--- events/onMember.js
+++ events/onMember.js
@@ -1,6 +1,8 @@
 /**
  * @param {import('discord.js').GuildMember} member
+ * @param {import('../classes/BotClient')} client
  */
-module.exports = (member) => {
+module.exports = (member, client) => {
   member.setNickname('[ 유저 ] ' + (member.nickname || member.user.username))
+  member.roles.add(client.settings.userRole)
 }
